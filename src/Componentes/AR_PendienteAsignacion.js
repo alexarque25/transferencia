8c69eda4d926e2a0039edffff546d2bf2a9c8021
alex andre arque <alex.arque@unmsm.edu.pe>
2019-12-10 23:48:30 -0500

    inicial

--- src/Componentes/AR_PendienteAsignacion.js
+++ src/Componentes/AR_PendienteAsignacion.js
@@ -30,11 +30,8 @@ class AR_PendienteAsignacion extends React.Component {
                                         <td className="td1">{recaudaciones.moneda}</td>
                                         <td className="td1">{recaudaciones.importe}</td>
                                         <td className="td1">{recaudaciones.fecha}</td>
-                                        
-                                        <td className="td1">
-                                                <AR_EstadoAsignacion estadoAsignacion={recaudaciones.observacion} />
-                                        </td>
-                                        
+                                        <td className="td1">{recaudaciones.observacion}</td>
+                                       
                                         <td className="td1">{recaudaciones.codAlumno}</td>
                                         
                                         <td className="td1">{recaudaciones.programa}</td>
